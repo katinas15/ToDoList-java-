2440025627047d48d9e169c5cb4f2d34ac8788f7
katinas15 <laimonas.janutenas@gmail.com>
2019-11-27 16:50:02 +0200

    progress

--- src/Controllers/WebController.java
+++ src/Controllers/WebController.java
@@ -4,38 +4,64 @@ package Controllers;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import ToDoMain.*;
+
 import org.springframework.http.HttpStatus;
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
 
 @Controller
+@SessionAttributes("user")
 public class WebController {
 
-    User currentUser = null;
     EntityManagerFactory factory = Persistence.createEntityManagerFactory("Lab4PU");
     UserHibernateController userHC = new UserHibernateController(factory);
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @ResponseStatus(value= HttpStatus.OK)
+    public String loginGet(){
+        return "login";
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseStatus(value= HttpStatus.OK)
-    @ResponseBody
-    public String login(@RequestBody String user){
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(User.class, new UserGSONSerializer());
-        Gson parser = gsonBuilder.create();
+    public String loginPost(@RequestParam(value="login") String login,
+                            @RequestParam(value="pass") String pass,
+                            ModelMap model){
 
-        User paduotas  = parser.fromJson(user, User.class);
-        User getUser = userHC.findUser(paduotas.getLogin());
+        User getUser = userHC.findUser(login);
         if(getUser != null){
-            if(paduotas.getLogin().equals(getUser.getLogin()) && paduotas.getPass().equals(getUser.getPass())){
-                return parser.toJson(getUser);
+            if(login.equals(getUser.getLogin()) && pass.equals(getUser.getPass())){
+                GsonBuilder gsonBuilder = new GsonBuilder();
+                gsonBuilder.registerTypeAdapter(User.class, new UserGSONSerializer());
+                Gson parser = gsonBuilder.create();
+
+                model.put("user", parser.toJson(getUser));
+                return "redirect:main";
             }
         }
-        return "Neteisingai ivesti duomenys";
+        model.addAttribute("error", true);
+        return "login";
+    }
+
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String main(@RequestParam(value = "user") String user, ModelMap model){
+        if(user.isEmpty()){
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(User.class, new UserGSONSerializer());
+            Gson parser = gsonBuilder.create();
+            model.addAttribute("kintamasis", user);
+        }
+        else model.addAttribute("kintamasis", "Welcome");
+        return "main";
     }
 
     @RequestMapping(value = "/full", method = RequestMethod.GET)
@@ -52,9 +78,15 @@ public class WebController {
     }
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index(ModelMap model){
-        model.addAttribute("kintamasis", 15);
-        return "index";
+    public String index(@ModelAttribute("user") User user, ModelMap model){
+        if(user != null){
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(User.class, new UserGSONSerializer());
+            Gson parser = gsonBuilder.create();
+            model.addAttribute("kintamasis", parser.toJson(user));
+        }
+        else model.addAttribute("kintamasis", "Welcome");
+        return "main";
     }
 
 
